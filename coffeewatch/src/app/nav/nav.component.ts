c3cfdb426d0a1e2b2e68ada3247f2b9f902dfc3a
johnzaro <johnzrgnns@gmail.com>
2019-02-28 20:09:37 +0200

    css fixes in new coffee shop page, added button for new coffee shop link

--- coffeewatch/src/app/nav/nav.component.ts
+++ coffeewatch/src/app/nav/nav.component.ts
@@ -1,31 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css']
 })
-export class NavComponent implements OnInit {
+export class NavComponent implements OnInit
+{
   loggedIN = undefined;
 
-
-  constructor(private userService: UserService) { }
+  constructor(
+    private userService: UserService,
+    private router: Router) { }
 
   ngOnInit() {
     console.log('NAV: checking login status');
-    this.loggedIN = this.userService.isloggedIN();
+    this.updateLoginStatus();
   }
 
   logout()
   {
     this.userService.logOut();
-    this.loggedIN = this.userService.isloggedIN();
+    this.updateLoginStatus();
   }
 
   updateLoginStatus()
   {
     this.loggedIN = this.userService.isloggedIN();
   }
-
 }
